7d3595bf2584af32b6a080b1428d1204794757af
Radek <radekbubaknet123@o2.pl>
2022-04-01 20:10:59 +0200

    Refactured JS

--- app.js
+++ app.js
@@ -1,71 +1,39 @@
 const btnToEnd = document.getElementById("BtnToThank");
 const votePage = document.getElementById("FirstPage");
 const thanksPage = document.getElementById("SecPage");
-let rate1 = document.getElementById("rateForOne");
-let rate2 = document.getElementById("rateForTwo");
-let rate3 = document.getElementById("rateForThree");
-let rate4 = document.getElementById("rateForFour");
-let rate5 = document.getElementById("rateForFive");
 const numberVoted = document.getElementById("VotedNumber");
-const checkIfClassActive = (z, c, b, n, m) => {
-	if (
-		m.classList.contains("actived") ||
-		n.classList.contains("actived") ||
-		b.classList.contains("actived") ||
-		c.classList.contains("actived")
-	) {
-		return (
-			m.classList.remove("actived"),
-			c.classList.remove("actived"),
-			b.classList.remove("actived"),
-			n.classList.remove("actived"),
-			z.classList.add("actived")
-		);
-	} else {
-		z.classList.add("actived");
-	}
-};
+const btns = document.querySelectorAll("button");
 
-const setNumber = num => {
-	if (num >= 1) {
-		numberVoted.textContent = `${num}`;
-	} 
-};
+for (const value of btns) {
+	value.addEventListener("click", e => {
+		if (value.classList.contains("actived")) {
+			for (let i = 0; i < btns.length; i++) {
+				btns.forEach(rem => rem.classList.remove("actived"));
+			}
+			return (
+				e.target.classList.add("actived"),
+				(numberVoted.textContent = e.target.textContent),
+				console.log(numberVoted.textContent)
+			);
+		} else {
+			btns.forEach(rem => rem.classList.remove("actived"));
+			e.target.classList.add("actived");
+			return (
+				(numberVoted.textContent = e.target.textContent),
+				console.log(numberVoted.textContent)
+			);
+		}
+	});
+}
 
-rate1.addEventListener("click", () => {
-	let number = 1;
-	checkIfClassActive(rate1, rate2, rate3, rate4, rate5);
-	return setNumber(number);
-});
-
-rate2.addEventListener("click", () => {
-	let number = 2;
-	checkIfClassActive(rate2, rate1, rate3, rate4, rate5);
-	return setNumber(number);
-});
-
-rate3.addEventListener("click", () => {
-	let number = 3;
-	checkIfClassActive(rate3, rate2, rate1, rate4, rate5);
-	return setNumber(number);
-});
-rate4.addEventListener("click", () => {
-	let number = 4;
-	checkIfClassActive(rate4, rate2, rate3, rate1, rate5);
-	return setNumber(number);
-});
-rate5.addEventListener("click", () => {
-	let number = 5;
-	checkIfClassActive(rate5, rate2, rate3, rate4, rate1);
-	return setNumber(number);
-});
-
-btnToEnd.addEventListener("click",
+btnToEnd.addEventListener(
+	"click",
 	(change = () => {
-		if ((votePage.style.display = "block")) votePage.style.display = "none";
+		if ((votePage.style.display = "block") && numberVoted.textContent !== "") votePage.style.display = "none";
 		thanksPage.style.display = "block";
 		if (numberVoted.textContent === "") {
-			numberVoted.textContent = "0";
+			alert("Please rate us!")
+			thanksPage.style.display = "none";
 		}
 	})
 );
